06b827d8cb1014a9905c251561671165b1f3f379
Samuel <torressam333@gmail.com>
2023-10-31 15:34:13 -0700

    Create and export Project model

---
+++ server/models/Project.ts
@@ -0,0 +1,32 @@
+/**Mongoose model for Project */
+import mongoose from 'mongoose';
+
+/** Project interface */
+export interface IProject {
+  name: string;
+  description: string;
+  status: 'Not Started' | 'In Progress' | 'Completed';
+  clientId: mongoose.Schema.Types.ObjectId;
+}
+
+const ProjectSchema = new mongoose.Schema<IProject>({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['Not Started', 'In Progress', 'Completed'],
+  },
+  clientId: {
+    // Always assign _id relating project ref -> client id
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Client',
+  },
+});
+
+export default mongoose.model<IProject>('Project', ProjectSchema);
