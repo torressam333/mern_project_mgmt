f5ea1c286c1fedaf6ef5e715cadb70124cd95a3c
Samuel <torressam333@gmail.com>
2023-11-12 14:57:32 -0700

    Add first passing unit test for client row component to check for prop

--- client/tests/components/ClientRow.test.tsx
+++ client/tests/components/ClientRow.test.tsx
@@ -1,9 +1,22 @@
 import { render, screen, waitFor } from '@testing-library/react';
+import ClientRow from '../../src/components/ClientRow';
 
 describe('Client Row', () => {
-  beforeEach(() => {});
+  const client = {
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    phone: '(123) 456-7890',
+  };
+
+  beforeEach(() => {
+    render(<ClientRow client={client} />);
+  });
 
   describe('Client Row UI', () => {
-    it('receives a client as a prop', () => {});
+    it('receives a client as a prop', () => {
+      expect(client).toHaveProperty('name');
+      expect(client).toHaveProperty('email');
+      expect(client).toHaveProperty('phone');
+    });
   });
 });
