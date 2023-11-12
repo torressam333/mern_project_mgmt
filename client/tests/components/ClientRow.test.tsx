885dc3ceb01944dbcd4286a0b0a5b7c3a887bbd6
Samuel <torressam333@gmail.com>
2023-11-12 14:54:58 -0700

    Init the unit tests for Client Row component

---
+++ client/tests/components/ClientRow.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen, waitFor } from '@testing-library/react';
+
+describe('Client Row', () => {
+  beforeEach(() => {});
+
+  describe('Client Row UI', () => {
+    it('receives a client as a prop', () => {});
+  });
+});
