b8e9660af9f227ddb42c4a71c6ddad60634e6e40
Samuel <torressam333@gmail.com>
2023-12-02 16:48:29 -0700

    Install faker library for more trustworthy seeding

--- server/seeders/clientSeeder.ts
+++ server/seeders/clientSeeder.ts
@@ -1,32 +1,11 @@
 import Client from '../models/Client';
-import { randomBytes } from 'crypto';
+import faker from 'faker';
 
-/**
- *
- * @param length
- * @returns
- */
-function generateRandomString(length: number): string {
-  return randomBytes(Math.ceil(length / 2))
-    .toString('hex')
-    .slice(0, length);
-}
-
-/**
- *
- * @param numClients
- * @param nameLength
- */
-async function seedClients(
-  numClients: number,
-  nameLength: number = 10
-): Promise<void> {
+async function seedClients(numClients: number): Promise<void> {
   const clients = Array.from({ length: numClients }, () => ({
-    name: generateRandomString(nameLength),
-    email: `${generateRandomString(5)}@example.com`,
-    phone: `(123) ${Math.floor(Math.random() * 1000)}-${Math.floor(
-      Math.random() * 10000
-    )}`,
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    phone: faker.phone.phoneNumber(),
   }));
 
   try {
@@ -37,5 +16,8 @@ async function seedClients(
     console.error(error);
   }
 }
+
+// Example usage:
+seedClients(100);
 
 export default seedClients;
